915838cff74ef4d0e940215d1429cd4b13f7b813
Stevanie Kanter <stevaniek@yahoo.com>
2021-10-16 23:16:28 -0400

    models update

--- models/index.js
+++ models/index.js
@@ -1,13 +1,32 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
+const Comments = require('./Comments')
 
-User.hasMany(Project, {
+User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Post.belongsTo(User, {
   foreignKey: 'user_id'
+});
+
+User.hasMany(Comments, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comments, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
+});
+
+Comments.belongsTo(Post, {
+  foreignKey: 'post_id',
 });
 
 module.exports = { User, Project };
