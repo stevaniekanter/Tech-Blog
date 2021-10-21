da2835d2c59f7d13c7ffafa426ab8cf9087231a1
Stevanie Kanter <stevaniek@yahoo.com>
2021-10-21 16:36:50 -0400

    added comments

---
+++ controllers/api/commentsRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { Comments } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const commentsData = await Comment.create({
+      comment:req.body.comments,
+      post_id:req.body.postId,
+      user_id:req.session.user_id ,
+    });
+    res.status(200).json(commentsData);
+  } catch (err) {
+    console.log(err)
+    res.status(400).json(err);
+  }
+});
+
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const postRoutes = require('./postRoutes');
+const commentsRoutes = require('./commentsRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentsRoutes);
 
 module.exports = router;
