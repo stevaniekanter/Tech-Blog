a416266a5440674426a5172f8b1f1361c5e517ed
Stevanie Kanter <stevaniek@yahoo.com>
2021-10-21 16:46:00 -0400

    fixed login

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#signup-username').value.trim();
+    const password = document.querySelector('#signup-password').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
+};
+
+document
+.querySelector('#signup-form')
+.addEventListener('submit', signupFormHandler);
