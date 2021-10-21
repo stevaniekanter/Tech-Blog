1d1d9cbd6af726b7ad4f7e60998eda158e531ed4
Stevanie Kanter <stevaniek@yahoo.com>
2021-10-21 16:32:45 -0400

    added routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,7 +5,7 @@ const withAuth = require('../utils/auth');
 router.get('/', withAuth, async (req, res) => {
   try {
     // Get all post 
-    const posttData = await Post.findAll({
+    const postData = await Post.findAll({
       include: [
         {
           model: User,
@@ -18,7 +18,9 @@ router.get('/', withAuth, async (req, res) => {
 
     res.render('homepage', { 
       posts, 
-      logged_in: req.session.logged_in 
+      logged_in: req.session.logged_in,
+      homeLink:'active',
+      titleHead:'Tech Blog'
     });
   } catch (err) {
     res.status(500).json(err);
@@ -26,26 +28,15 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 router.get('/login', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      project,
-      sponsors: [{ name: "Google" }],
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return
   }
+
+  res.render('login', {
+    homeLink:'active',
+    titleHead:'Tech Blog'
+  });
 });
 
 // Use withAuth middleware to prevent access to route
@@ -78,4 +69,89 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// GET one post
+router.get('/post/:id', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id);
+    const postInfo = dbPostData.get({ plain: true });
+
+    const commentData = await Comment.findAll(
+      { where: 
+        { user_id: req.session.user_id,post_id:req.params.id } ,
+        include:[
+          {
+            model: User
+          }
+        ]
+      }
+      );
+    const comments = commentData.map((comments) =>
+      comments.get({ plain: true })
+    );
+    res.render('post', { 
+      postInfo, 
+      comments,
+      loggedIn: req.session.loggedIn
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET new post
+router.get('/new-post', withAuth, async (req, res) => {
+  try {
+    
+    res.render('partials/add-post', { 
+      loggedIn: req.session.loggedIn,
+      dashboardLink:" active ",
+      titleHead:'Your Dashboard' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/edit-post/:id', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findByPk(req.params.id);
+    const postInfo = dbPostData.get({ plain: true });
+    
+    res.render('partials/edit-post', { 
+      postInfo, 
+      loggedIn: req.session.loggedIn,
+      dashboardLink:" active ",
+      titleHead:'Your Dashboard' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({ where: { user_id: req.session.user_id } });
+    const posts = postData.map((postInfo) =>
+      postInfo.get({ plain: true })
+    );
+    res.render('dashboard', {
+      posts,
+      logged_in: req.session.logged_in,
+      dashboardLink:" active ",
+      titleHead:'Your Dashboard'
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
 module.exports = router;
